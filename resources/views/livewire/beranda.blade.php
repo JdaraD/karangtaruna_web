edc597fadfdac59f7d06bc36f508aaa97a1c7f1c
JdaraD <Juliandara17@gmail.com>
2025-05-02 10:58:12 +0700

    update gallery progress, gallery karang taruna

--- resources/views/livewire/beranda.blade.php
+++ resources/views/livewire/beranda.blade.php
@@ -16,9 +16,9 @@
         {{-- title --}}
 
         {{-- content --}}
-        <div class="flex justify-center bg-white w-full h-full"> 
+        <div class="flex justify-center bg-white w-full h-full">
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 py-4 ">
-                
+
                 <div class="flex flex-col items-center bg-[#6A9C89] w-[280px] h-[370px] rounded-lg">
 
                     <img src="{{ asset('image/program.jpg') }}" alt="" class="w-[250px] h-[180px] mt-4 rounded-lg">
@@ -34,7 +34,7 @@
                             <p class="capitalize font-[poppins] font-medium text-xs line-clamp-6 px-4 mt-2 text-justify select-none">
                                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
                             </p>
-                            
+
                         </div>
                         <div class="flex justify-center items-center row-span-1">
                             <a href="" class="flex justify-center bg-[#2E8A99] hover:bg-[#1F5B64] text-white font-[poppins] font-medium text-xs w-full mx-2 py-1 rounded-sm capitalize">Kunjungi.....</a>
@@ -45,7 +45,7 @@
                 <div class="flex flex-col items-center bg-[#6A9C89] w-[280px] h-[370px] rounded-lg">
 
                     <img src="{{ asset('image/program.jpg') }}" alt="" class="w-[250px] h-[180px] mt-4 rounded-lg">
-                    
+
                     <div class="grid grid-rows-6 my-2 h-[180px]">
                         <div class="flex justify-center items-center row-span-1">
                             <p class="capitalize font-[poppins] font-medium text-sm">
@@ -57,53 +57,7 @@
                             <p class="capitalize font-[poppins] font-medium text-xs line-clamp-6 px-4 mt-2 text-justify select-none">
                                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
                             </p>
-                            
-                        </div>
-                        <div class="flex justify-center items-center row-span-1">
-                            <a href="" class="flex justify-center bg-[#2E8A99] hover:bg-[#1F5B64] text-white font-[poppins] font-medium text-xs w-full mx-2 py-1 rounded-sm capitalize">Kunjungi.....</a>
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="flex flex-col items-center bg-[#6A9C89] w-[280px] h-[370px] rounded-lg">
 
-                    <img src="{{ asset('image/program.jpg') }}" alt="" class="w-[250px] h-[180px] mt-4 rounded-lg">
-                    
-                    <div class="grid grid-rows-6 my-2 h-[180px]">
-                        <div class="flex justify-center items-center row-span-1">
-                            <p class="capitalize font-[poppins] font-medium text-sm">
-                                test
-                            </p>
-
-                        </div>
-                        <div class="row-span-4">
-                            <p class="capitalize font-[poppins] font-medium text-xs line-clamp-6 px-4 mt-2 text-justify select-none">
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                            </p>
-                            
-                        </div>
-                        <div class="flex justify-center items-center row-span-1">
-                            <a href="" class="flex justify-center bg-[#2E8A99] hover:bg-[#1F5B64] text-white font-[poppins] font-medium text-xs w-full mx-2 py-1 rounded-sm capitalize">Kunjungi.....</a>
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="flex flex-col items-center bg-[#6A9C89] w-[280px] h-[370px] rounded-lg">
-
-                    <img src="{{ asset('image/program.jpg') }}" alt="" class="w-[250px] h-[180px] mt-4 rounded-lg">
-                    
-                    <div class="grid grid-rows-6 my-2 h-[180px]">
-                        <div class="flex justify-center items-center row-span-1">
-                            <p class="capitalize font-[poppins] font-medium text-sm">
-                                test
-                            </p>
-
-                        </div>
-                        <div class="row-span-4">
-                            <p class="capitalize font-[poppins] font-medium text-xs line-clamp-6 px-4 mt-2 text-justify select-none">
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                            </p>
-                            
                         </div>
                         <div class="flex justify-center items-center row-span-1">
                             <a href="" class="flex justify-center bg-[#2E8A99] hover:bg-[#1F5B64] text-white font-[poppins] font-medium text-xs w-full mx-2 py-1 rounded-sm capitalize">Kunjungi.....</a>
@@ -114,7 +68,7 @@
                 <div class="flex flex-col items-center bg-[#6A9C89] w-[280px] h-[370px] rounded-lg">
 
                     <img src="{{ asset('image/program.jpg') }}" alt="" class="w-[250px] h-[180px] mt-4 rounded-lg">
-                    
+
                     <div class="grid grid-rows-6 my-2 h-[180px]">
                         <div class="flex justify-center items-center row-span-1">
                             <p class="capitalize font-[poppins] font-medium text-sm">
@@ -126,7 +80,7 @@
                             <p class="capitalize font-[poppins] font-medium text-xs line-clamp-6 px-4 mt-2 text-justify select-none">
                                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
                             </p>
-                            
+
                         </div>
                         <div class="flex justify-center items-center row-span-1">
                             <a href="" class="flex justify-center bg-[#2E8A99] hover:bg-[#1F5B64] text-white font-[poppins] font-medium text-xs w-full mx-2 py-1 rounded-sm capitalize">Kunjungi.....</a>
@@ -137,7 +91,7 @@
                 <div class="flex flex-col items-center bg-[#6A9C89] w-[280px] h-[370px] rounded-lg">
 
                     <img src="{{ asset('image/program.jpg') }}" alt="" class="w-[250px] h-[180px] mt-4 rounded-lg">
-                    
+
                     <div class="grid grid-rows-6 my-2 h-[180px]">
                         <div class="flex justify-center items-center row-span-1">
                             <p class="capitalize font-[poppins] font-medium text-sm">
@@ -149,7 +103,53 @@
                             <p class="capitalize font-[poppins] font-medium text-xs line-clamp-6 px-4 mt-2 text-justify select-none">
                                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
                             </p>
-                            
+
+                        </div>
+                        <div class="flex justify-center items-center row-span-1">
+                            <a href="" class="flex justify-center bg-[#2E8A99] hover:bg-[#1F5B64] text-white font-[poppins] font-medium text-xs w-full mx-2 py-1 rounded-sm capitalize">Kunjungi.....</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex flex-col items-center bg-[#6A9C89] w-[280px] h-[370px] rounded-lg">
+
+                    <img src="{{ asset('image/program.jpg') }}" alt="" class="w-[250px] h-[180px] mt-4 rounded-lg">
+
+                    <div class="grid grid-rows-6 my-2 h-[180px]">
+                        <div class="flex justify-center items-center row-span-1">
+                            <p class="capitalize font-[poppins] font-medium text-sm">
+                                test
+                            </p>
+
+                        </div>
+                        <div class="row-span-4">
+                            <p class="capitalize font-[poppins] font-medium text-xs line-clamp-6 px-4 mt-2 text-justify select-none">
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                            </p>
+
+                        </div>
+                        <div class="flex justify-center items-center row-span-1">
+                            <a href="" class="flex justify-center bg-[#2E8A99] hover:bg-[#1F5B64] text-white font-[poppins] font-medium text-xs w-full mx-2 py-1 rounded-sm capitalize">Kunjungi.....</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex flex-col items-center bg-[#6A9C89] w-[280px] h-[370px] rounded-lg">
+
+                    <img src="{{ asset('image/program.jpg') }}" alt="" class="w-[250px] h-[180px] mt-4 rounded-lg">
+
+                    <div class="grid grid-rows-6 my-2 h-[180px]">
+                        <div class="flex justify-center items-center row-span-1">
+                            <p class="capitalize font-[poppins] font-medium text-sm">
+                                test
+                            </p>
+
+                        </div>
+                        <div class="row-span-4">
+                            <p class="capitalize font-[poppins] font-medium text-xs line-clamp-6 px-4 mt-2 text-justify select-none">
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                            </p>
+
                         </div>
                         <div class="flex justify-center items-center row-span-1">
                             <a href="" class="flex justify-center bg-[#2E8A99] hover:bg-[#1F5B64] text-white font-[poppins] font-medium text-xs w-full mx-2 py-1 rounded-sm capitalize">Kunjungi.....</a>
@@ -178,25 +178,37 @@
         <div class="relative flex flex-col">
 
             <div class="flex items-center justify-center bg-white w-full h-full">
-                <div class="flex flex-wrap justify-center gap-6 py-4 max-w-[1400px]">
-                    <div class="flex justify-center items-center w-[442px] h-[249px] bg-[#F5F5F5] rounded-lg">
+                <div class="flex flex-wrap justify-center gap-3 py-4 max-w-[1440px]">
+                    <div class="flex justify-center items-center w-[420px] h-[249px] bg-[#F5F5F5] rounded-lg">
+                        <div class="grid grid-rows-1 gap-2 h-full w-full rounded-lg px-2 py-2">
+                            <div class="flex flex-col flex-wrap gap-2 h-[164px] rounded-lg">
+                                <div class="flex h-[164px] w-[144px]">
+                                    <img src="{{ asset('image/pp.jpg') }}" alt="" class="object-fit rounded-lg">
+                                </div>
+                                <div class="h-[164px] w-[250px] bg-red-500 rounded-lg">
+                                    <p>test</p>
+                                </div>
+                            </div>
+                            <div class="bg-red-500 h-[60px] rounded-lg">
+                                <p>test</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex justify-center items-center w-[420px] h-[249px] bg-[#F5F5F5] rounded-lg">
                         <p>test</p>
                     </div>
-                    <div class="flex justify-center items-center w-[442px] h-[249px] bg-[#F5F5F5] rounded-lg">
+                    <div class="flex justify-center items-center w-[420px] h-[249px] bg-[#F5F5F5] rounded-lg">
                         <p>test</p>
                     </div>
-                    <div class="flex justify-center items-center w-[442px] h-[249px] bg-[#F5F5F5] rounded-lg">
+                    <div class="flex justify-center items-center w-[420px] h-[249px] bg-[#F5F5F5] rounded-lg">
                         <p>test</p>
                     </div>
-                    <div class="flex justify-center items-center w-[442px] h-[249px] bg-[#F5F5F5] rounded-lg">
-                        <p>test</p>
-                    </div>
-                    <div class="flex justify-center items-center w-[442px] h-[249px] bg-[#F5F5F5] rounded-lg">
+                    <div class="flex justify-center items-center w-[420px] h-[249px] bg-[#F5F5F5] rounded-lg">
                         <p>test</p>
                     </div>
                 </div>
             </div>
-    
+
             <div class="flex justify-center items-center h-[50px] w-full px-4">
                 <a href="" class="flex items-center justify-center bg-[#2E8A99] font-[poppins] text-sm w-full rounded-sm font-[poppins] text-sm text-white">Data Selengkapnya</a>
             </div>
@@ -208,14 +220,66 @@
     {{-- gallery progress --}}
 
     {{-- gallery karang taruna --}}
-    <div>
-        <p>test</p>
+    <div class="relative flex justify-center flex-col">
+        {{-- title --}}
+        <div class="flex justify-center py-4">
+            <p class="uppercase font-[poppins] text-[30px] font-bold">
+                gallery karang taruna
+            </p>
+        </div>
+        {{-- title --}}
+
+        {{-- content --}}
+        <div class="relative">
+
+            <div class="flex justify-center mb-4">
+
+                <div class="flex justify-center w-[80%] gap-4">
+
+                    {{-- <div class="bg-[#F5F5F5] w-[25%] h-[330px] rounded-lg"> --}}
+                        <img src="{{ asset('image/pp.jpg')}}" alt="nama folder" class="w-[25%] h-[330px] objeh-fullct-fit rounded-lg">
+                    {{-- </div> --}}
+
+                    <div class="grid grid-cols-3 gap-4 w-[50%] h-[330px]">
+                        <div class="bg-gray-500 w-full h-[157px] col-span-2 rounded-lg">
+                            <img src="{{ asset('image/ln1.jpg')}}" alt="" class="h-full w-full object-cover rounded-lg">
+                        </div>
+                        <div class="bg-gray-500 w-full h-[157px] rounded-lg">
+                            <img src="{{ asset('image/ln2.jpeg')}}" alt="" class="h-full w-full object-cover rounded-lg">
+                        </div>
+                        <div class="bg-gray-500 w-full h-[157px] col-span-3 rounded-lg">
+                            <img src="{{ asset('image/ln3.jpeg')}}" alt="" class="h-full w-full object-cover rounded-lg">
+                        </div>
+                    </div>
+
+                    {{-- <div class="bg-[#F5F5F5] w-[25%] h-[330px] rounded-lg"> --}}
+                        <img src="{{ asset('image/pp1.jpg')}}" alt="" class="w-[25%] h-[330px] object-fit rounded-lg">
+                    {{-- </div> --}}
+
+                </div>
+
+            </div>
+
+            <div class="flex justify-center items-center h-[50px] w-full px-4">
+                <a href="" class="flex items-center justify-center bg-[#2E8A99] font-[poppins] text-sm w-full rounded-sm font-[poppins] text-sm text-white">Data Selengkapnya</a>
+            </div>
+
+        </div>
+        {{-- content --}}
+
+
     </div>
     {{-- gallery karang taruna --}}
 
     {{-- kontak --}}
-    <div>
-        <p>test</p>
+    <div class="relative flex justify-center flex-col">
+        {{-- title --}}
+        <div class="flex justify-center py-4">
+            <p class="uppercase font-[poppins] text-[30px] font-bold">
+                kontak
+            </p>
+        </div>
+        {{-- title --}}
     </div>
     {{-- kontak --}}
 
